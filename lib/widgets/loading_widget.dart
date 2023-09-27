f2f208f8e1aba586407e0dde8cf1fe3345a13a46
oagarian <tns6@aluno.ifal.edu.br>
2023-09-27 13:51:46 +0000

    Timer gif

--- lib/widgets/loading_widget.dart
+++ lib/widgets/loading_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gif/flutter_gif.dart';
 
 class LoadingCat extends StatefulWidget {
   const LoadingCat({super.key});
@@ -7,9 +8,28 @@ class LoadingCat extends StatefulWidget {
   State<LoadingCat> createState() => _LoadingCatState();
 }
 
-class _LoadingCatState extends State<LoadingCat> {
+class _LoadingCatState extends State<LoadingCat> with TickerProviderStateMixin{
+    late FlutterGifController controller;
+
+    void initState() {
+      super.initState();
+      controller = FlutterGifController(vsync: this);
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+      controller.repeat(
+        min: 0,
+        max: 82,
+        period: const Duration(milliseconds: 1500),
+      );
+    });
+    }
+
   @override
   Widget build(BuildContext context) {
-    return Image.asset('assets/images/loading_cat.gif', width: 150, height: 150);
+    return GifImage(
+      controller: controller,
+      image: const AssetImage('assets/images/loading_cat.gif'),
+      width: 150,
+      height: 150,
+    );
   }
 }
